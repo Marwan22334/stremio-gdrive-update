90aafef00f6f80d8cf95b05c66c1331a83631a65
BeastAcc <121842363+BeastAcc@users.noreply.github.com>
2023-01-05 19:35:36 +0530

    Update __init__.py

--- sgd/__init__.py
+++ sgd/__init__.py
@@ -4,7 +4,8 @@ from flask import Flask
 from sgd.gdrive import GoogleDrive
 
 app = Flask(__name__)
-token = json.loads(os.environ.get("TOKEN"))
+tokenFromVar=os.environ.get('TOKEN')
+token = json.loads(tokenFromVar)
 gdrive = GoogleDrive(token)
 
 from sgd import routes
